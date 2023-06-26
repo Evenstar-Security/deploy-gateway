0717ce7ee67f7b63ae5e83e6b1ab6d3c7509af42
Wesley Belleman <bellemanwesley@gmail.com>
2023-06-25 23:14:11 -0400

    trying something new

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+pwd;
 runuser -l  ec2-user -c 'git pull origin';
 runuser -l  ec2-user -c 'git -C ../company-website pull origin';
 sudo cp -r ../company-website/* /usr/share/nginx/html/;
