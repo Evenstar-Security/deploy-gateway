8e536ee0330ac420add356912356d920353a1a0d
Wesley Belleman <bellemanwesley@gmail.com>
2023-06-25 23:07:13 -0400

    syntax fixes

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 git pull origin;
-git pull -C ../company-website origin;
-cp ../company-website/* /usr/share/nginx/html/;
+git -C ../company-website pull origin;
+cp -r ../company-website/* /usr/share/nginx/html/;
